15d85e7f826111b08424a7b3550d964664c64a18
Youssef Aboalyouser <youssefaboalyouser@gmail.com>
2026-04-24 19:47:45 +0300

    bug solved

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
 # Load from environment for security (recommended)
-VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY", "f9be6b631de6c89ca11824db80a89fab5d25b6051f92bde177c1b7aa92ac5d1c")
+VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
+
+if not VIRUSTOTAL_API_KEY:
+    raise ValueError("API key not found in environment variables")
 
 VT_BASE_URL = "https://www.virustotal.com/api/v3"
 
